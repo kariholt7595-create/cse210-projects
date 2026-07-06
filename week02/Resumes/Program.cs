261669c6f70f9fc14ae727d9c54f6d5389fe0e5c
kariholt7595-create <kariholt7595@gmail.com>
2026-07-05 20:02:12 -0600

    Created Job first

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Console.WriteLine(job1._company);
+
+
+        Job job2 = new Job();
+
+        job2._company = "Apple";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+
+        Console.WriteLine(job2._company);
     }
 }
