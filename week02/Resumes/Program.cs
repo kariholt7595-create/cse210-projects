2274de4d408c7e31ecd1777f843e7333ec114b3f
kariholt7595-create <kariholt7595@gmail.com>
2026-07-05 21:45:47 -0600

    Finished with Resume and completed assignment

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,22 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-
         job1._company = "Microsoft";
         job1._jobTitle = "Software Engineer";
         job1._startYear = 2019;
         job1._endYear = 2022;
 
-        Console.WriteLine(job1._company);
-
 
         Job job2 = new Job();
-
         job2._company = "Apple";
         job2._jobTitle = "Software Engineer";
         job2._startYear = 2022;
-        job2._endYear = 2025;
+        job2._endYear = 2023;
 
-        Console.WriteLine(job2._company);
+
+        Resume myResume = new Resume();
+
+        myResume._personName = "Karissa Holt";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
