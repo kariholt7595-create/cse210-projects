31904e2299d73cf74fea250f7f8dcab5123fba2f
kariholt7595-create <kariholt7595@gmail.com>
2026-07-17 20:59:53 -0600

    Finished Scripture Memorizer

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+
+// ***Exceeding requirements*** The user can choose how many words are hidden during each round, allowing them to adjust the memorization difficulty.
+// I also made it so the program can only hide words that aren't already hidden.
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+
+        Scripture scripture = new Scripture(reference, "Trust in the Lord with all thine heart and lean not unto thine own understanding.");
+
+        // I added this for exceeding requirements
+        Console.Write("How many words would you like to hide each round? ");
+        int wordsToHide = int.Parse(Console.ReadLine());
+
+        string userInput = "";
+
+        while (userInput != "quit" && !scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
+
+            userInput = Console.ReadLine();
+
+            if (userInput != "quit")
+            {
+                scripture.HideRandomWords(wordsToHide);
+                // This line is changed for exceeding requirements 
+            }
+        }
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
     }
 }
--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel;
 
 public class Scripture
 {
@@ -8,21 +9,59 @@ public class Scripture
 
     public Scripture(Reference reference, string text)
     {
-        
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] words = text.Split(' ');
+
+        foreach (string word in words)
+        {
+            _words.Add(new Word(word));
+        }
     }
 
+    // Made some changes to this block of code to adjust for it only choosing words that aren't hidden already.
     public void HideRandomWords(int numberToHide)
     {
-        
+        Random random = new Random();
+
+        for (int i = 0; i < numberToHide; i++)
+        {
+            if (IsCompletelyHidden())
+            {
+                return;
+            }
+
+            int index = random.Next(_words.Count);
+
+            while (_words[index].IsHidden())
+            {
+                index = random.Next(_words.Count);
+            }
+            _words[index].Hide();
+        }
     }
 
     public string GetDisplayText()
     {
-        return "";
+        string scriptureText = _reference.GetDisplayText() + " ";
+        foreach (Word word in _words)
+        {
+            scriptureText += word.GetDisplayText() + " ";
+        }
+        return scriptureText;
     }
 
     public bool IsCompletelyHidden()
     {
-        return false;
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
