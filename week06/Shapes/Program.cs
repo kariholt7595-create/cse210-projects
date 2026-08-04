b0bb85f3ee3c6438485481ad1847a655395630a2
kariholt7595-create <kariholt7595@gmail.com>
2026-08-03 20:22:48 -0600

    Shapes learning assignment finished

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+    
+        shapes.Add(new Square("Blue", 5));
+        shapes.Add(new Rectangle("Red", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }            
     }
 }
